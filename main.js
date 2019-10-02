72d3d4343de1680076e616678e3f01131f3e8c96
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-02 15:05:58 -0400

    finished exercise

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-function isHot() {
+function isHot(temp) {
+  return temp >= 80 ? "Yes, it is indeed hot." : "No, it is not hot."
 
 }
 
 
-function helloThere() {
+function helloThere(name) {
+  return name.length >5 ? "Hi, " + name : "Hello, " + name;
 
 }
 
 
-function goodbyeYou() {
+function goodbyeYou(name) {
+  
+  
+  return name = name ? "Goodbye, " + name : "Goodbye, stranger"; 
 
 }
+
+
+
 
 
 module.exports = {
